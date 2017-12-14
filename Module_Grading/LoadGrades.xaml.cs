418cac7d1a1e2d17a6725b1b093a4e5fc0fd87c1
Borlagdan <hugeklutz@gmail.com>
2017-12-15 04:14:43 +0800

    Initial setup for Grading module

---
+++ Module_Grading/LoadGrades.xaml.cs
@@ -0,0 +1,81 @@
+﻿using MahApps.Metro.Controls;
+using System.Data;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using Excel;
+
+namespace Module_Grading
+{
+    /// <summary>
+    /// Interaction logic for LoadGrades.xaml
+    /// </summary>
+    public partial class LoadGrades : MetroWindow
+    {
+        string buttonContent;
+
+        public LoadGrades()
+        {
+            InitializeComponent();
+            
+            buttonContent = "Open";
+        }
+
+        DataSet results;
+
+        private void btn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            cb_WorkBooks.SelectedIndex = -1;
+
+            if (buttonContent.ToString() == "Open")
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Excel Workbook 97-2003|*.xls|Excel Workbook|*.xlsx";
+                ofd.ValidateNames = true;
+
+                if (ofd.ShowDialog() == true)
+                {
+                    FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    IExcelDataReader reader;
+                    
+                    if(ofd.FilterIndex == 1)
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(fs);
+                    }
+
+                    else
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+                    }
+                    
+                    reader.IsFirstRowAsColumnNames = true;
+                    results = reader.AsDataSet();
+
+                    cb_WorkBooks.Items.Clear();
+
+                    foreach (DataTable dt in results.Tables)
+                    {
+                        cb_WorkBooks.Items.Add(dt.TableName);
+
+                        reader.Close();
+                    }
+
+                    btn_Open.Content = "Load";
+                }
+
+                else if (buttonContent.ToString() == "Load")
+                {
+                    dgv_Grades.Items.Clear();
+                }
+            }
+        }
+
+        private void cb_WorkBooks_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (cb_WorkBooks.SelectedIndex != -1)
+            {
+                dgv_Grades.ItemsSource = results.Tables[cb_WorkBooks.SelectedValue.ToString()].DefaultView;
+            }
+        }
+    }
+}
---
+++ Module_Grading/Login.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace Module_Grading
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : MetroWindow
+    {
+        private static string dbLocation = "server=localhost;user id=root;database=cfcsf_capstone";
+        private string dbQuery;
+        
+        public Login()
+        {
+            InitializeComponent();
+
+            txt_UserName.Focus();
+        }
+
+        private void btn_Login_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(txt_UserName.Text == null)
+            {
+                this.ShowMessageAsync("Enter Username to login.", "Error");
+                txt_UserName.Focus();
+            }
+
+            else if (txt_PassWord.Password.ToString() == null)
+            {
+                this.ShowMessageAsync("Enter Password to login.", "Error");
+                txt_PassWord.Focus();
+            }
+
+            else
+            {
+                db_ValidateLoginInformation();
+            }
+        }
+
+        private void db_ValidateLoginInformation()
+        {
+            MySqlConnection dbConnection = new MySqlConnection(dbLocation);
+
+            dbQuery = "SELECT username, password FROM useraccounts WHERE username = '" + txt_UserName.Text + "' AND password = '" + txt_PassWord.Password.ToString() + "' AND usertype = 'Grade Encoder'";
+
+            MySqlCommand dbCommand = new MySqlCommand(dbQuery, dbConnection);
+
+            try
+            {
+                dbConnection.Open();
+
+                MySqlDataReader dbDataReader = dbCommand.ExecuteReader();
+
+                if (dbDataReader.Read() == true)
+                {
+                    dbConnection.Close();
+
+                    // this.ShowMessageAsync("Welcome " + txt_UserName.Text, "Login successful");
+
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+
+                    this.Close();
+                }
+
+                else
+                {
+                    dbConnection.Close();
+
+                    // this.ShowMessageAsync("Incorrect Login credentials.", "Error");
+                    txt_UserName.Focus();
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                dbConnection.Close();
+            }
+
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+    }
+}
